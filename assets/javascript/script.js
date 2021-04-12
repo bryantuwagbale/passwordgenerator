e67475dd5f1971e845a012f9650e8aae8df2fac0
bryantuwagbale <78828750+bryantuwagbale@users.noreply.github.com>
2021-04-11 20:08:04 -0500

    final changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,20 @@
+// function that syncs the slider and the number input
+range.addEventListener("input", sync)
+number.addEventListener("input", sync)
+function sync(e) {
+  var value = e.target.value 
+  number.value = value
+  range.value = value
+}
+
+
+// function that generates random password from string of characters 
 function getPassword () {
 
+  // list of numbers, characters, and symbols to pull from
   var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(_+?><:{[]})";
   var passwordLength = 12
-  var password = "";
+  var password = ""; 
 
   for (var i = 0; i < passwordLength; i++){
     var randomNumber = Math.floor(Math.random()* chars.length);
@@ -11,3 +23,6 @@ function getPassword () {
   document.getElementById("password").value = password
 }
 
+
+// not exactly sure how to "connect" the checkboxes and output of the password just yet, so I'm just using a randomized list
+// of everything
